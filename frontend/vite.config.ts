423bf43f130b17be78494a14df76940fd91e91b3
Quackcoder14 <dhanushgit1411@gmail.com>
2026-09-25 01:11:15 +0530

    bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
+  root: fileURLToPath(new URL(".", import.meta.url)),
   server: {
     port: 3000,
     strictPort: true,
